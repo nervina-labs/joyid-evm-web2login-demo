c63193d4f94e0a96ef54039c75b4bbec95a65143
yuche <i@yuche.me>
2024-01-16 15:03:42 +0800

    fix

--- index.tsx
+++ index.tsx
@@ -7,7 +7,7 @@ import '@unocss/reset/tailwind.css'
 import 'uno.css'
 
 initConfig({
-  joyidAppURL: "https://testnet.joyid.dev",
+  joyidAppURL: "https://joyid-app-git-fix-web2login-nervina.vercel.app",
   backgroundImage: `center center / cover no-repeat url("https://images.unsplash.com/photo-1601314167099-232775b3d6fd?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`
 });
 
